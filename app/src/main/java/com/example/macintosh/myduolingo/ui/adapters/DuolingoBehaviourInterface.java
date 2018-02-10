6399c18424322114532536c282ac28627fb086a3
Stephany Daneri <sdaneri@belatrixsf.com>
2018-02-10 13:05:27 -0500

    Adding Languages List using RecyclerView

---
+++ app/src/main/java/com/example/macintosh/myduolingo/ui/adapters/DuolingoBehaviourInterface.java
@@ -0,0 +1,9 @@
+package com.example.macintosh.myduolingo.ui.adapters;
+
+/**
+ * Created by macintosh on 2/10/18.
+ */
+
+public  interface DuolingoBehaviourInterface {
+    void chooseLanguageToLearn(int languageDescPosition, String languageName);
+}
