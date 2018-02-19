e212cd67b369949246ebc145d51471622fda56fb
Stephany Daneri <sdaneri@belatrixsf.com>
2018-02-19 10:49:24 -0500

    Adding more home elements

--- app/src/main/java/com/example/macintosh/myduolingo/UserSessionDashboard.java
+++ app/src/main/java/com/example/macintosh/myduolingo/UserSessionDashboard.java
@@ -1,17 +1,32 @@
 package com.example.macintosh.myduolingo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-public class UserSessionDashboard extends AppCompatActivity {
+import com.example.macintosh.myduolingo.ui.OnFragmentInteractionListener;
+import com.example.macintosh.myduolingo.ui.fragments.Fragment1;
+import com.example.macintosh.myduolingo.ui.fragments.Fragment2;
+import com.example.macintosh.myduolingo.ui.fragments.Fragment3;
+
+public class UserSessionDashboard extends AppCompatActivity implements OnFragmentInteractionListener {
 
     Intent navIntent;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
     //private LanguageToLearnEntity languageToLearnObj;
 
 
@@ -19,22 +34,92 @@ public class UserSessionDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_session_dashboard);
-        getUserSessionInfo();
+//        getUserSessionInfo();
         ui();
     }
 
-    private void getUserSessionInfo() {
-        navIntent = getIntent();
-        //languageToLearnObj = (LanguageToLearnEntity) navIntent.getSerializableExtra("STUDIED_LANGUAGE");
-    }
+//    private void getUserSessionInfo() {
+//        navIntent = getIntent();
+//        //languageToLearnObj = (LanguageToLearnEntity) navIntent.getSerializableExtra("STUDIED_LANGUAGE");
+//    }
 
     private void ui() {
-        Toolbar myToolbar = findViewById(R.id.myToolbar);
-        setSupportActionBar(myToolbar);
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
 
         TextView tviLangToBeLearned = findViewById(R.id.tviToolBarTitle);
         //tviLangToBeLearned.setText(languageToLearnObj.getLangName());
         tviLangToBeLearned.setText("Inglés");
+
+        // - - DRAWER - -
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()){
+
+
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.menu1:
+                        changeFragment(0);
+                        return true;
+
+                    // For rest of the options we just show a toast on click
+
+                    case R.id.menu2:
+                        //Toast.makeText(getApplicationContext(),"Stared Selected",Toast.LENGTH_SHORT).show();
+                        changeFragment(1);
+                        return true;
+                    case R.id.menu3:
+                        //Toast.makeText(getApplicationContext(),"Send Selected",Toast.LENGTH_SHORT).show();
+                        changeFragment(2);
+                        return true;
+
+                    default:
+                        return true;
+
+                }
+
+            }
+        });
+
     }
 
     @Override
@@ -43,6 +128,27 @@ public class UserSessionDashboard extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.language_progress_menu, menu);
         return true;
     }
+
+    private void changeFragment(int i) {
+        Fragment fragment = null;
+        switch (i){
+            case 0:
+                fragment = (Fragment) new Fragment1();
+                break;
+            case 1:
+                fragment = (Fragment) new Fragment2();
+                break;
+            case 2:
+                fragment = (Fragment) new Fragment3();
+                break;
+        }
+        if(fragment!=null){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.frame,fragment);
+            fragmentTransaction.commit();
+        }
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -58,5 +164,10 @@ public class UserSessionDashboard extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/example/macintosh/myduolingo/home_nav_drawer.java
@@ -0,0 +1,95 @@
+package com.example.macintosh.myduolingo;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.macintosh.myduolingo.ui.OnFragmentInteractionListener;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link home_nav_drawer.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link home_nav_drawer#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class home_nav_drawer extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public home_nav_drawer() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment home_nav_drawer.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static home_nav_drawer newInstance(String param1, String param2) {
+        home_nav_drawer fragment = new home_nav_drawer();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_nav_drawer, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
